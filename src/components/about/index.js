1fc556f2a040db5d3ef59b32f7f13be72ba7a22c
Guo <gggzzz@126.com>
2019-10-16 13:32:00 +0800

    [update] use $state in ctrl

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,4 +2,9 @@ import angular from 'angular';
 
 angular.module('main').component('about', {
   template: require('./about.html'),
+  controller($state) {
+    this.jumpToPeople = function jumpToPeople() {
+      $state.go('people');
+    };
+  },
 });
